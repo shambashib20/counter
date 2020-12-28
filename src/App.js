713aa8c38773f768af0d10731ada74e9f28c3fc5
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-28 11:38:17 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <CountUp />
+      <CountUp end={200} />
+      <br />
+      <CountUp end={200} duration={5} />
+
     </div>
   );
 }
